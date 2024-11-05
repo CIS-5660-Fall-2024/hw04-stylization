4ff83f66e0c932f8c0be6eea38a9838872e2ee9d
jichumao <jichumao@qq.com>
2024-11-05 12:06:23 -0500

    finished

--- Assets/Scripts/Turntable.cs
+++ Assets/Scripts/Turntable.cs
@@ -6,6 +6,9 @@ public class Turntable : MonoBehaviour
 {
 
     public float rotationSpeed = 1.0f;
+    public float maxRotationAngle = 15.0f;
+    private float currentAngle = 0.0f;
+    private int direction = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,38 @@ public class Turntable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+        //this.transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+
+        float deltaAngle = rotationSpeed * Time.deltaTime * direction;
+        currentAngle += deltaAngle;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (rotationSpeed == 1.0)
+            {
+                rotationSpeed = 10.0f;
+
+            }else if(rotationSpeed == 10.0f)
+            {
+                rotationSpeed = 20.0f;
+            }
+            else
+            {
+                rotationSpeed = 1.0f;
+            }
+        }
+
+        if (currentAngle > maxRotationAngle)
+        {
+            currentAngle = maxRotationAngle;
+            direction = -1; 
+        }
+        else if (currentAngle < -maxRotationAngle)
+        {
+            currentAngle = -maxRotationAngle;
+            direction = 1;
+        }
+
+        transform.Rotate(0, deltaAngle, 0);
     }
 }
--- Assets/Shaders/Includes/OutlineHelp.hlsl
+++ Assets/Shaders/Includes/OutlineHelp.hlsl
@@ -10,3 +10,49 @@ void GetNormal_float(float2 uv, out float3 Normal)
 {
     Normal = SAMPLE_TEXTURE2D(_NormalsBuffer, sampler_point_clamp, uv).rgb;
 }
+
+static float2 sobelSamplePoints[9] = {
+    float2(-1, 1),float2(0, 1),float2(1, 1),
+	float2(-1, 0),float2(0, 0),float2(1, 1),
+	float2(-1, -1),float2(0, -1),float2(1, -1)
+};
+
+static float sobelXMatrix[9] = {
+	-1, 0, 1,
+	-2, 0, 2,
+	-1, 0, 1,
+};
+
+static float sobelYMatrix[9] = {
+	1, 2, 1,
+	0, 0, 0,
+	-1, -2, -1,
+};
+
+float SimpleNoise(float2 uv)
+{
+    return frac(sin(dot(uv, float2(12.9898, 78.233))) * 43758.5453);
+}
+
+void DepthSobel_float(float2 UV, float Thickness,float time,float distortion,out float Out) {
+    float2 sobel = 0;
+	[unroll] for (int i = 0; i < 9; i++) {
+		float depth = 0;
+		// 0 - 1
+		float noiseValue = SimpleNoise(UV + sobelSamplePoints[i]);
+        float2 noiseOffset = float2(sin(time + noiseValue), cos(time + noiseValue)) * 1.4;
+
+		GetDepth_float(UV + sobelSamplePoints[i] * Thickness + time * distortion * noiseOffset, depth);
+		sobel += depth * float2(sobelXMatrix[i], sobelYMatrix[i]);
+	}
+	Out = length(sobel);
+}
+
+void ACESFilm_float(float3 InColor, out float3 OutColor) {
+    float a = 2.51;
+    float b = 0.03;
+    float c = 2.43;
+    float d = 0.59;
+    float e = 0.14;
+    OutColor = saturate((InColor* (a * InColor + b)) / (InColor * (c * InColor + d) + e));
+}
